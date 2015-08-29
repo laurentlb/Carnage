c263830356630474befaf4d543d90128aea07dd6
Laurent Le Brun <laurentlb@gmail.com>
2015-08-29 13:57:23 +0200

    Fix mine & dynamite, some code cleanup (use switch case)

--- rules.js
+++ rules.js
@@ -10,19 +10,20 @@ var allDirections = [
     ];
 
 function newGame() {
-    // TODO
+    // TODO: init game at random
     return makeDummyState();
 }
 
 function move(state, d) { // active worm is moved by player
-    if (state.movesLeft > 0) {
-	state.movesLeft -= 1;
-	var w = state.selectedWorm;
-	var worm = state.worms[w];
-	state.trail.push({x:worm.x, y:worm.y});
-	moveWorm(state, w, addXY(worm, d));
-	return state;
+    if (state.movesLeft <= 0) {
+        fail("no moves left");
     }
+    state.movesLeft -= 1;
+    var w = state.selectedWorm;
+    var worm = state.worms[w];
+    state.trail.push({x:worm.x, y:worm.y});
+    moveWorm(state, w, addXY(worm, d));
+    return state;
 }
 
 function moveWorm(state, wormId, p) {
@@ -49,7 +50,7 @@ function exists(state, x, y) {
 
 function canMove(state, d) {
     if (state.movesLeft <= 0) {
-	return false;
+        return false;
     }
     var w = state.selectedWorm;
     var x = state.worms[w].x + d.x;
@@ -80,6 +81,8 @@ function harm(state, w) {
 }
 
 function endTurn(state) {
+    // TODO: mine, dynamite
+    // TODO: draw card
     state.selectedCard = -1;
     state.selectedWorm = -1;
     return state;
@@ -104,85 +107,89 @@ function canAttack(state, c) {
     }
 
     var activeWorm = state.worms[state.selectedWorm];
-    var clickedWorm = c.worm < 0 ? null : state.worms[c.worm];
-    var direction = findDirection(activeWorm, c.worm < 0 ? c : clickedWorm);
-    var distance = distanceBetween(activeWorm, c.worm < 0 ? c : clickedWorm);
+    var targetWorm = c.worm < 0 ? null : state.worms[c.worm];
+    var direction = findDirection(activeWorm, c.worm < 0 ? c : targetWorm);
+    var distance = distanceBetween(activeWorm, c.worm < 0 ? c : targetWorm);
     var nearestTarget = direction === null ? null : nearestWorm(state, activeWorm, direction);
 
-    if (state.actions[state.selectedCard] === "Shotgun") {
-	return clickedWorm !== null && direction !== null && nearestTarget === clickedWorm;
-    } else if (state.actions[state.selectedCard] === "Bow") {
-	return clickedWorm !== null && direction !== null && nearestTarget === clickedWorm;
-    } else if (state.actions[state.selectedCard] === "Baseball Bat") {
-	return clickedWorm !== null && direction !== null && distance === 1;
-    } else if (state.actions[state.selectedCard] === "Pistol") {
-    } else if (state.actions[state.selectedCard] === "Flame Thrower") {
-    } else if (state.actions[state.selectedCard] === "Kamikaze") {
-    } else if (state.actions[state.selectedCard] === "Hook") {
-    } else if (state.actions[state.selectedCard] === "Mine") {
-	return containsXY(state.trail, c);
-    } else if (state.actions[state.selectedCard] === "Dynamite") {
-	return containsXY(state.trail, c);
-    } else if (state.actions[state.selectedCard] === "") {
+    switch (state.actions[state.selectedCard]) {
+    case "Shotgun":
+        return nearestTarget !== null && nearestTarget === targetWorm;
+    case "Bow":
+        return nearestTarget !== null && nearestTarget === targetWorm;
+    case "Baseball Bat":
+        return targetWorm !== null && distance === 1;
+    case "Mine":
+        return containsXY(state.trail, c);
+    case "Dynamite":
+        return containsXY(state.trail, c);
+    case "Pistol":
+    case "Flame Thrower":
+    case "Kamikaze":
+    case "Hook":
+    default:
+        fail(state.actions[state.selectedCard] + " not implemented");
     }
-    // TODO
-    return true;
 }
 
 function attack(state, c) {
     var activeWorm = state.worms[state.selectedWorm];
-    var clickedWorm = c.worm < 0 ? null : state.worms[c.worm];
-    var direction = findDirection(activeWorm, c.worm < 0 ? c : clickedWorm);
-    var distance = distanceBetween(activeWorm, c.worm < 0 ? c : clickedWorm);
+    var targetWorm = c.worm < 0 ? null : state.worms[c.worm];
+    var direction = findDirection(activeWorm, c.worm < 0 ? c : targetWorm);
+    var distance = distanceBetween(activeWorm, c.worm < 0 ? c : targetWorm);
     var nearestTarget = direction === null ? null : nearestWorm(state, activeWorm, direction);
-    if (state.actions[state.selectedCard] === "Shotgun") {
-	if (clickedWorm !== null && direction !== null && nearestTarget === clickedWorm) {
-	    push(state, clickedWorm, direction);
-	    if (clickedWorm.life > 0) {
-		harm(state, clickedWorm);
-	    }
-	}
-    } else if (state.actions[state.selectedCard] === "Bow") {
-	if (clickedWorm !== null && direction !== null && nearestTarget === clickedWorm) {
-	    push(state, clickedWorm, direction);
-	    push(state, clickedWorm, direction);
-	}
-    } else if (state.actions[state.selectedCard] === "Baseball Bat") {
-	if (clickedWorm !== null && direction !== null && distance === 1) {
-	    push(state, clickedWorm, direction);
-	    push(state, clickedWorm, direction);
-	    push(state, clickedWorm, direction);
-	}
-    } else if (state.actions[state.selectedCard] === "Pistol") {
-    } else if (state.actions[state.selectedCard] === "Flame Thrower") {
-    } else if (state.actions[state.selectedCard] === "Kamikaze") {
-    } else if (state.actions[state.selectedCard] === "Hook") {
-    } else if (state.actions[state.selectedCard] === "Mine") {
-	board[c.x][c.y].mine = true;
-    } else if (state.actions[state.selectedCard] === "Dynamite") {
-	board[c.x][c.y].dynamite = true;
-    } else if (state.actions[state.selectedCard] === "") {
-    } else {
-	// TODO
-	harm(state, state.worms[c.worm]);
+
+    switch (state.actions[state.selectedCard]) {
+    case "Shotgun":
+        assert(nearestTarget === targetWorm);
+        push(state, targetWorm, direction);
+        if (targetWorm.life > 0) {
+            harm(state, targetWorm);
+        }
+        break;
+    case "Bow":
+        assert(nearestTarget === targetWorm);
+        push(state, targetWorm, direction);
+        push(state, targetWorm, direction);
+        break;
+    case "Baseball Bat":
+        assert(distance == 1);
+        push(state, targetWorm, direction);
+        push(state, targetWorm, direction);
+        push(state, targetWorm, direction);
+        break;
+    case "Mine":
+        state.board[c.x][c.y].mine = true;
+        break;
+    case "Dynamite":
+        state.board[c.x][c.y].dynamite = true;
+        break;
+    case "Pistol":
+    case "Flame Thrower":
+    case "Kamikaze":
+    case "Hook":
+    default:
+        fail(state.actions[state.selectedCard] + " not implemented");
     }
+
+    // TODO: remove card
     return state;
 }
 
 function push(state, worm, direction) {
     checkargs(state, worm, direction);
     if (worm.life < 1) {
-	return;
+        return;
     }
     var behind = addXY(worm, direction);
     if (!exists(state, behind.x, behind.y)) {
-	kill(state, worm);
+        kill(state, worm);
     } else {
-	var wormBehind = findWormAt(state, behind);
-	if (wormBehind !== null) {
-	    push(state, wormBehind, direction);
-	}
-	moveWorm(state, wormId(state, worm), addXY(worm, direction));
+        var wormBehind = findWormAt(state, behind);
+        if (wormBehind !== null) {
+            push(state, wormBehind, direction);
+        }
+        moveWorm(state, wormId(state, worm), addXY(worm, direction));
     }
 }
 
@@ -201,11 +208,11 @@ function nearestWorm(state, from, direction) {
     checkargs(state, from, direction);
     var p = addXY(from, direction);
     if (!exists(state, p.x, p.y)) {
-	return null;
+        return null;
     }
     var worm = findWormAt(state, p);
     if (worm !== null) {
-	return worm;
+        return worm;
     }
     return nearestWorm(state, p, direction);
 }
@@ -223,24 +230,24 @@ function distanceBetween(from, to) {
     checkargs(from, to);
     var dx = to.x - from.x, dy = to.y - from.y;
     if (dx * dy > 0) {
-	return Math.max(Math.abs(dx), Math.abs(dy));
+        return Math.max(Math.abs(dx), Math.abs(dy));
     } else {
-	return Math.abs(dx) + Math.abs(dy);
+        return Math.abs(dx) + Math.abs(dy);
     }
 }
 
 function findDirection(from, to) {
     checkargs(from, to);
     if (!isLinedUpWith(from, to)) {
-	return null;
+        return null;
     }
     var dx = to.x - from.x, dy = to.y - from.y;
     if (dx === dy) {
-	return dx > 0 ? {x:1,y:1} : {x:-1,y:-1};
+        return dx > 0 ? {x:1,y:1} : {x:-1,y:-1};
     } else if (dx === 0) {
-	return dy > 0 ? {x:0,y:1} : {x:0,y:-1};
+        return dy > 0 ? {x:0,y:1} : {x:0,y:-1};
     } else {
-	return dx > 0 ? {x:1,y:0} : {x:-1,y:0};
+        return dx > 0 ? {x:1,y:0} : {x:-1,y:0};
     }
 }
 
@@ -251,10 +258,10 @@ function addXY(p1, p2) {
 
 function containsXY(list, p) {
     for (var i = 0; i < list.length; ++i) {
-	var item = list[i];
-	if (item.x === p.X && item.y === p.y) {
-	    return true;
-	}
+        var item = list[i];
+        if (item.x === p.x && item.y === p.y) {
+            return true;
+        }
     }
     return false;
 }
@@ -267,14 +274,20 @@ function fail(message) {
     throw 'Assertion failed! ' + message;
 }
 
+function assert(bool) {
+    if (!bool) {
+        fail("assert failed");
+    }
+}
+
 function checkargs() { // varargs
     for (var i = 0; i < arguments.length; ++i) {
-	var arg = arguments[i];
-	if (arg === undefined) {
-	    fail('argument is undefined.');
-	}
-	if (arg === null) {
-	    fail('argument is null.');
-	}
+        var arg = arguments[i];
+        if (arg === undefined) {
+            fail('argument is undefined.');
+        }
+        if (arg === null) {
+            fail('argument is null.');
+        }
     }
 }
